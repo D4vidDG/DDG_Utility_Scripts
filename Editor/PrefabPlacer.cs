ae5149309f2590f22efddbcbc6c026a366808c3e
David <jdiazga@unal.edu.co>
2025-05-23 12:04:05 -0500

    Add editor tools

---
+++ Editor/PrefabPlacer.cs
@@ -0,0 +1,467 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering;
+using System.Collections.Generic;
+
+public class PrefabPlacer : EditorWindow
+{
+    private const float MIN_RADIUS = 0.1f;
+    private const float OFFSET = 2;
+    private const float RADIUS_SCROLL_INCREMENT = 0.1f;
+    private const int DISC_RESOLUTION = 50;
+    private const float DISC_WIDTH = 1f;
+    private const float TEST_RAY_DISTANCE_LIMIT = 40;
+
+    public float circleRadius = 10;
+    public int count = 10;
+    public float spawnRadius = 2;
+    public Material previewMaterial;
+    public Transform parent;
+
+
+    SerializedObject so;
+    SerializedProperty propRadius;
+    SerializedProperty propCount;
+    SerializedProperty propPreviewMaterial;
+    SerializedProperty propParent;
+    SerializedProperty propSpawnRadius;
+
+
+
+    class CirclePoint
+    {
+        public Vector2 point;
+        public bool used;
+
+        public CirclePoint(Vector2 point, bool used)
+        {
+            this.point = point;
+            this.used = used;
+        }
+    }
+
+
+    List<CirclePoint> pointsInCircle;
+    List<CirclePoint> randomPointsInCircle;
+
+    GameObjectSpawnInfo[] gameObjectsSpawnInfo;
+
+    Dictionary<GameObject, MeshFilter[]> meshesByPrefab;
+    Dictionary<GameObject, bool> isPrefabSelected;
+    bool gameObjectsSelected;
+
+    [MenuItem("Tools/Prefab Placer")]
+    private static void ShowWindow()
+    {
+
+        var window = GetWindow<PrefabPlacer>();
+        window.titleContent = new GUIContent("Prefab Placer");
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        so = new SerializedObject(this);
+        meshesByPrefab = new Dictionary<GameObject, MeshFilter[]>();
+        isPrefabSelected = new Dictionary<GameObject, bool>();
+
+        propRadius = so.FindProperty("circleRadius");
+        propCount = so.FindProperty("count");
+        propPreviewMaterial = so.FindProperty("previewMaterial");
+        propParent = so.FindProperty("parent");
+        propSpawnRadius = so.FindProperty("spawnRadius");
+
+
+        SceneView.duringSceneGui += DuringSceneGUI;
+
+        GenerateRandomPointsInCircle();
+        SelectRandomPointsInCircle();
+        LoadPrefabs();
+    }
+
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= DuringSceneGUI;
+    }
+
+
+    private void OnGUI()
+    {
+        so.Update();
+        EditorGUILayout.PropertyField(propRadius);
+        EditorGUILayout.PropertyField(propSpawnRadius);
+
+        propRadius.floatValue = Mathf.Max(propRadius.floatValue, MIN_RADIUS);
+        propSpawnRadius.floatValue = Mathf.Min(propSpawnRadius.floatValue, propRadius.floatValue);
+        if (so.ApplyModifiedProperties())
+        {
+            GenerateRandomPointsInCircle();
+            SelectRandomPointsInCircle();
+        }
+
+        so.Update();
+        EditorGUILayout.PropertyField(propCount);
+        propCount.intValue = Mathf.Max(propCount.intValue, 0);
+        if (so.ApplyModifiedProperties())
+        {
+            SelectRandomPointsInCircle();
+        }
+
+
+        so.Update();
+        EditorGUILayout.PropertyField(propPreviewMaterial);
+        if (so.ApplyModifiedProperties())
+        {
+            SceneView.RepaintAll();
+        };
+
+        so.Update();
+        EditorGUILayout.PropertyField(propParent);
+        so.ApplyModifiedProperties();
+
+
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            DeselectUI();
+        }
+
+        if (!gameObjectsSelected)
+        {
+            EditorGUILayout.HelpBox("No GameObjects assigned", MessageType.Warning, true);
+        }
+    }
+
+
+
+    private void DuringSceneGUI(SceneView sceneView)
+    {
+
+        Handles.BeginGUI();
+        Rect rect = new Rect(8, 8, 50, 50);
+        foreach (GameObject gameObject in meshesByPrefab.Keys)
+        {
+            Texture icon = AssetPreview.GetAssetPreview(gameObject);
+            isPrefabSelected[gameObject] = GUI.Toggle(rect, isPrefabSelected[gameObject], icon);
+            if (GUI.changed)
+            {
+                gameObjectsSelected = AreGameObjectsSelected();
+                SelectRandomPointsInCircle();
+                Repaint();
+            }
+
+            rect.x += rect.width + 5;
+        }
+
+        Handles.EndGUI();
+
+        if (gameObjectsSelected)
+        {
+            DisplaySceneGUI(sceneView);
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            {
+                InstantiateGameObjects();
+                SelectRandomPointsInCircle();
+                Event.current.Use();
+            }
+
+            PreviewGameObjects(sceneView.camera);
+        }
+
+        //Increase radius with input
+        TryIncreaseRadius();
+    }
+
+    private void DisplaySceneGUI(SceneView sceneView)
+    {
+        Handles.zTest = CompareFunction.LessEqual;
+        Transform camTf = sceneView.camera.transform;
+        Ray rayToMouse = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+        if (Physics.Raycast(rayToMouse, out RaycastHit mouseRayHitInfo))
+        {
+            Vector3 normal = mouseRayHitInfo.normal;
+            Vector3 tangent = Vector3.Cross(normal, camTf.up).normalized;
+            Vector3 biTangent = Vector3.Cross(normal, tangent);
+
+            DrawDiscOnSurface(mouseRayHitInfo.point, circleRadius, normal, tangent);
+
+
+            for (int i = 0; i < randomPointsInCircle.Count; i++)
+            {
+                Vector3 pointInRadius =
+                    mouseRayHitInfo.point + (randomPointsInCircle[i].point.x * tangent + randomPointsInCircle[i].point.y * biTangent);
+
+
+                Ray testRay = GetOffsetRayTo(pointInRadius, mouseRayHitInfo.normal);
+
+                if (Physics.Raycast(testRay, out RaycastHit surfaceInfo, TEST_RAY_DISTANCE_LIMIT))
+                {
+                    if (gameObjectsSpawnInfo[i] == null)
+                    {
+                        gameObjectsSpawnInfo[i] = new GameObjectSpawnInfo(surfaceInfo, SelectRandomGameObject());
+                        DrawSphere(surfaceInfo.point, spawnRadius);
+
+                    }
+                    else gameObjectsSpawnInfo[i].surfaceInfo = surfaceInfo;
+                }
+            }
+
+            HandleUtility.Repaint();
+        }
+    }
+
+    private void DrawDiscOnSurface(Vector3 origin, float radius, Vector3 normal, Vector3 tangent)
+    {
+        Handles.color = Color.white;
+        Vector3[] points = new Vector3[DISC_RESOLUTION];
+        float angle = 0;
+        for (int i = 0; i < DISC_RESOLUTION; i++)
+        {
+            Vector3 testRayOrigin = origin + (Quaternion.AngleAxis(angle, normal) * tangent * radius) + normal * OFFSET;
+            Ray testRay = new Ray(testRayOrigin, -normal);
+            if (Physics.Raycast(testRay, out RaycastHit surfaceHit, TEST_RAY_DISTANCE_LIMIT))
+            {
+                points[i] = surfaceHit.point;
+            }
+            angle += 360 / DISC_RESOLUTION;
+        }
+
+        Handles.DrawAAPolyLine(DISC_WIDTH, points);
+    }
+
+    private void InstantiateGameObjects()
+    {
+        for (int i = 0; i < gameObjectsSpawnInfo.Length; i++)
+        {
+            if (!IsValidPosition(gameObjectsSpawnInfo[i])) continue;
+            GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(gameObjectsSpawnInfo[i].gameObject);
+            Undo.RegisterCreatedObjectUndo(go, "Spawn Prefab");
+            go.transform.position = gameObjectsSpawnInfo[i].surfaceInfo.point;
+            go.transform.rotation = gameObjectsSpawnInfo[i].rotation;
+            go.transform.parent = parent;
+        }
+    }
+
+    private void PreviewGameObjects(Camera cam)
+    {
+        if (gameObjectsSpawnInfo == null) return;
+
+        for (int i = 0; i < gameObjectsSpawnInfo.Length; i++)
+        {
+            if (gameObjectsSpawnInfo[i] == null) continue;
+            if (meshesByPrefab[gameObjectsSpawnInfo[i].gameObject] == null) continue;
+
+            Quaternion rotation = gameObjectsSpawnInfo[i].rotation;
+
+
+            previewMaterial.SetPass(0);
+
+            foreach (MeshFilter filter in meshesByPrefab[gameObjectsSpawnInfo[i].gameObject])
+            {
+                if (!IsValidPosition(gameObjectsSpawnInfo[i])) continue;
+
+                if (filter.sharedMesh == null) continue;
+
+                Matrix4x4 rootToWorld =
+                    Matrix4x4.TRS(
+                    gameObjectsSpawnInfo[i].surfaceInfo.point,
+                    gameObjectsSpawnInfo[i].rotation,
+                    Vector3.one
+                    );
+
+                Matrix4x4 localToRoot = filter.transform.localToWorldMatrix;
+
+                Matrix4x4 localToWorld = rootToWorld * localToRoot;
+
+                Graphics.DrawMesh(filter.sharedMesh, localToWorld, previewMaterial, 0, cam);
+            }
+
+        }
+    }
+
+    private void TryIncreaseRadius()
+    {
+        so.Update();
+        bool holdingAlt = (Event.current.modifiers & EventModifiers.Alt) != 0;
+        if (Event.current.type == EventType.ScrollWheel && holdingAlt)
+        {
+            propRadius.floatValue *= 1 + Mathf.Sign(Event.current.delta.y) * RADIUS_SCROLL_INCREMENT;
+            Event.current.Use();
+            Repaint();
+        }
+        if (so.ApplyModifiedProperties())
+        {
+            GenerateRandomPointsInCircle();
+            SelectRandomPointsInCircle();
+        }
+    }
+
+    private void GenerateRandomPointsInCircle()
+    {
+        pointsInCircle = new List<CirclePoint>();
+        float radius = 0;
+        float angle = 0;
+        Vector2 testPoint = new Vector2();
+
+        int numberOfRadiusIncreases = 0;
+
+
+        while (IsValidPointInCircle(radius, angle, out testPoint))
+        {
+            CirclePoint circlePoint = new CirclePoint(testPoint, false);
+            pointsInCircle.Add(circlePoint);
+
+            if (angle >= 360 * Mathf.Deg2Rad || radius == 0)
+            {
+                angle = 0;
+                radius += 2 * spawnRadius;
+                numberOfRadiusIncreases++;
+            }
+            else
+            {
+                angle += (60 * Mathf.Deg2Rad) / numberOfRadiusIncreases;
+            }
+        }
+
+    }
+
+    private void SelectRandomPointsInCircle()
+    {
+        if (randomPointsInCircle != null)
+        {
+            foreach (CirclePoint cp in randomPointsInCircle)
+            {
+                cp.used = false;
+            }
+        }
+
+        randomPointsInCircle = new List<CirclePoint>();
+        gameObjectsSpawnInfo = new GameObjectSpawnInfo[count];
+
+        int pointsSelected = 0;
+        int maxNumberOfPoints = Mathf.Min(count, pointsInCircle.Count);
+        while (pointsSelected < maxNumberOfPoints)
+        {
+            CirclePoint randPoint = pointsInCircle[Random.Range(0, pointsInCircle.Count)];
+            if (!randPoint.used)
+            {
+                pointsSelected++;
+                randPoint.used = true;
+                randomPointsInCircle.Add(randPoint);
+            }
+        }
+
+    }
+
+    private bool IsValidPointInCircle(float radius, float angle, out Vector2 point)
+    {
+        point = radius * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        bool insideCircle = (point.x * point.x + point.y * point.y) <= (circleRadius * circleRadius);
+        if (insideCircle) return true;
+        return false;
+    }
+
+    private void LoadPrefabs()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:prefab", new string[] { "Assets/Prefabs" });
+        string[] paths = new string[guids.Length];
+        for (int i = 0; i < guids.Length; i++)
+        {
+            paths[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(paths[i]);
+            meshesByPrefab.Add(prefab, prefab.GetComponentsInChildren<MeshFilter>());
+            isPrefabSelected.Add(prefab, false);
+        }
+    }
+
+    private static Ray GetOffsetRayTo(Vector3 origin, Vector3 direction)
+    {
+        return new Ray(origin + direction * OFFSET, -direction);
+    }
+
+    private bool AreGameObjectsSelected()
+    {
+        return isPrefabSelected.ContainsValue(true);
+    }
+
+    private GameObject SelectRandomGameObject()
+    {
+        List<GameObject> prefabs = new List<GameObject>(isPrefabSelected.Keys);
+        int randomIndex = Random.Range(0, prefabs.Count);
+        GameObject selectedGO = prefabs[randomIndex];
+        if (!isPrefabSelected[selectedGO]) return SelectRandomGameObject();
+        return selectedGO;
+    }
+
+    private bool IsValidPosition(GameObjectSpawnInfo spawnInfo)
+    {
+        Ray ray = new Ray(spawnInfo.surfaceInfo.point, spawnInfo.surfaceInfo.normal);
+        float meshHeight = CalculateHeight(spawnInfo.gameObject);
+        return !Physics.Raycast(ray, meshHeight);
+    }
+
+    private float CalculateHeight(GameObject gameObject)
+    {
+        float totalHeight = 0;
+        foreach (MeshRenderer renderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            float meshHeight = (renderer.bounds.center.y + renderer.bounds.extents.y);
+            if (meshHeight <= totalHeight) continue;
+            float extraHeight = (meshHeight - totalHeight);
+            totalHeight += extraHeight;
+        }
+        return totalHeight;
+    }
+
+    private void DeselectUI()
+    {
+        GUI.FocusControl(null);
+        Repaint();
+    }
+
+    private void DrawSphere(Vector3 pointPos, float radius)
+    {
+        Handles.SphereHandleCap(1, pointPos, Quaternion.identity, radius, EventType.Repaint);
+    }
+}
+
+
+class GameObjectSpawnInfo
+{
+    public RaycastHit surfaceInfo
+    {
+        get
+        {
+            return _hitInfo;
+        }
+        set
+        {
+            _hitInfo = value;
+            GenerateRotation();
+        }
+
+    }
+
+    public GameObject gameObject;
+    public Quaternion rotation;
+    public float height;
+
+    private Vector3 randVec;
+    private RaycastHit _hitInfo;
+
+
+    public GameObjectSpawnInfo(RaycastHit hitInfo, GameObject gameObject)
+    {
+        randVec = Random.insideUnitSphere;
+        this.surfaceInfo = hitInfo;
+        this.gameObject = gameObject;
+    }
+
+    private void GenerateRotation()
+    {
+        Vector3 randForward = Vector3.Cross(surfaceInfo.normal, randVec);
+        rotation = Quaternion.LookRotation(randForward, surfaceInfo.normal);
+    }
+}
---
+++ Editor/SnapperTool.cs
@@ -0,0 +1,292 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+
+[CanEditMultipleObjects]
+public class SnapperTool : EditorWindow
+{
+
+    const float MIN_SCALE = 0.1f;
+    const float MIN_DIMENSION = 1f;
+
+    public float width = 10;
+    public float height = 2;
+    public float depth = 10;
+    public float xScale = 1;
+    public float yScale = 1;
+    public float zScale = 1;
+    public float radius = 10;
+    public float radiusScale = 1;
+    public float angularScale = 10;
+
+
+    public GridOriginOptions gridOrigin = GridOriginOptions.World;
+    public GridShapeOptions gridShape = GridShapeOptions.Cartesian;
+
+
+
+
+    SerializedProperty propWidth;
+    SerializedProperty propHeight;
+    SerializedProperty propDepth;
+    SerializedProperty propXScale;
+    SerializedProperty propYScale;
+    SerializedProperty propZScale;
+    SerializedProperty propRadius;
+    SerializedProperty propRadiusScale;
+    SerializedProperty propAngularScale;
+    SerializedProperty propGridOrigin;
+    SerializedProperty propGridShape;
+
+    SerializedObject so;
+
+    public enum GridOriginOptions
+    {
+        Selection,
+        World,
+    }
+
+    public enum GridShapeOptions
+    {
+        Radial,
+        Cartesian,
+    }
+
+    private void OnEnable()
+    {
+        so = new SerializedObject(this);
+
+        propWidth = so.FindProperty("width");
+        propHeight = so.FindProperty("height");
+        propDepth = so.FindProperty("depth");
+        propXScale = so.FindProperty("xScale");
+        propYScale = so.FindProperty("yScale");
+        propZScale = so.FindProperty("zScale");
+        propRadius = so.FindProperty("radius");
+        propRadiusScale = so.FindProperty("radiusScale");
+        propAngularScale = so.FindProperty("angularScale");
+        propGridOrigin = so.FindProperty("gridOrigin");
+        propGridShape = so.FindProperty("gridShape");
+
+        Selection.selectionChanged += Repaint;
+        SceneView.duringSceneGui += DuringSceneGUI;
+    }
+
+    private void DuringSceneGUI(SceneView sceneView)
+    {
+        Handles.zTest = CompareFunction.LessEqual;
+        if (Event.current.type == EventType.Repaint)
+        {
+            if (gridShape == GridShapeOptions.Cartesian) DisplayCartesianGrid(GetGridOrigin());
+            if (gridShape == GridShapeOptions.Radial) DisplayRadialGrid(GetGridOrigin());
+        }
+    }
+
+
+
+    private void OnDisable()
+    {
+        Selection.selectionChanged -= Repaint;
+        SceneView.duringSceneGui -= DuringSceneGUI;
+    }
+
+    [MenuItem("Tools/Snapper")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<SnapperTool>();
+        window.titleContent = new GUIContent("Snapper");
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        so.Update();
+        EditorGUILayout.PropertyField(propGridShape);
+        so.ApplyModifiedProperties();
+
+        so.Update();
+        switch (gridShape)
+        {
+            case GridShapeOptions.Cartesian:
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.LabelField("Dimensions:");
+                    EditorGUILayout.PropertyField(propWidth);
+                    EditorGUILayout.PropertyField(propHeight);
+                    EditorGUILayout.PropertyField(propDepth);
+                }
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.LabelField("Scale:");
+                    EditorGUILayout.PropertyField(propXScale);
+                    EditorGUILayout.PropertyField(propYScale);
+                    EditorGUILayout.PropertyField(propZScale);
+                }
+
+                propWidth.floatValue = Mathf.Max(propWidth.floatValue, MIN_DIMENSION);
+                propHeight.floatValue = Mathf.Max(propHeight.floatValue, MIN_DIMENSION);
+                propDepth.floatValue = Mathf.Max(propDepth.floatValue, MIN_DIMENSION);
+                propXScale.floatValue = Mathf.Max(Mathf.Min(propXScale.floatValue, propWidth.floatValue / 2), MIN_SCALE);
+                propYScale.floatValue = Mathf.Max(Mathf.Min(propYScale.floatValue, propHeight.floatValue / 2), MIN_SCALE);
+                propZScale.floatValue = Mathf.Max(Mathf.Min(propZScale.floatValue, propDepth.floatValue / 2), MIN_SCALE);
+                break;
+
+            case GridShapeOptions.Radial:
+
+                EditorGUILayout.PropertyField(propRadius);
+                EditorGUILayout.PropertyField(propRadiusScale);
+                EditorGUILayout.PropertyField(propAngularScale);
+
+
+                propRadius.floatValue = Mathf.Max(propRadius.floatValue, MIN_DIMENSION);
+                propRadiusScale.floatValue = Mathf.Max(Mathf.Min(propRadiusScale.floatValue, propRadius.floatValue / 2), MIN_SCALE);
+                propAngularScale.floatValue = Mathf.Max(Mathf.Min(propAngularScale.floatValue, 180), MIN_SCALE);
+                break;
+        }
+
+
+
+        EditorGUILayout.PropertyField(propGridOrigin);
+
+        so.ApplyModifiedProperties();
+
+
+
+        using (new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0))
+        {
+            if (GUILayout.Button("Snap Selection"))
+            {
+                SnapSelection(gridShape);
+            }
+        }
+    }
+
+    private void DisplayCartesianGrid(Vector3 origin)
+    {
+
+        Handles.color = Color.blue;
+        Handles.DrawLine(new Vector3(width / 2, 0, depth / 2) + origin, new Vector3(-width / 2, 0, depth / 2) + origin);
+        Handles.DrawLine(new Vector3(-width / 2, 0, depth / 2) + origin, new Vector3(-width / 2, 0, -depth / 2) + origin);
+        Handles.DrawLine(new Vector3(-width / 2, 0, -depth / 2) + origin, new Vector3(width / 2, 0, -depth / 2) + origin);
+        Handles.DrawLine(new Vector3(width / 2, 0, -depth / 2) + origin, new Vector3(width / 2, 0, depth / 2) + origin);
+
+
+        Handles.color = Color.white;
+
+        for (int i = 0; i < Mathf.Floor(height / (2 * yScale)); i++)
+        {
+            float yOffset = i * yScale;
+
+            for (int j = 0; j < Mathf.Floor((width) / (2 * xScale)) + 1; j++)
+            {
+                float xOffset = j * xScale;
+                Handles.DrawLine(new Vector3(xOffset, yOffset, -depth / 2) + origin, new Vector3(xOffset, yOffset, depth / 2) + origin);
+                Handles.DrawLine(new Vector3(-xOffset, yOffset, -depth / 2) + origin, new Vector3(-xOffset, yOffset, depth / 2) + origin);
+                if (yOffset == 0) continue;
+                Handles.DrawLine(new Vector3(xOffset, -yOffset, -depth / 2) + origin, new Vector3(xOffset, -yOffset, depth / 2) + origin);
+                Handles.DrawLine(new Vector3(-xOffset, -yOffset, -depth / 2) + origin, new Vector3(-xOffset, -yOffset, depth / 2) + origin);
+            }
+
+            for (int j = 0; j < Mathf.Floor((depth) / (2 * zScale)) + 1; j++)
+            {
+                float zOffset = j * zScale;
+                Handles.DrawLine(new Vector3(-width / 2, yOffset, zOffset) + origin, new Vector3(width / 2, yOffset, zOffset) + origin);
+                Handles.DrawLine(new Vector3(-width / 2, yOffset, -zOffset) + origin, new Vector3(width / 2, yOffset, -zOffset) + origin);
+                if (yOffset == 0) continue;
+                Handles.DrawLine(new Vector3(-width / 2, -yOffset, zOffset) + origin, new Vector3(width / 2, -yOffset, zOffset) + origin);
+                Handles.DrawLine(new Vector3(-width / 2, -yOffset, -zOffset) + origin, new Vector3(width / 2, -yOffset, -zOffset) + origin);
+            }
+            HandleUtility.Repaint();
+        }
+    }
+
+
+    private void DisplayRadialGrid(Vector3 origin)
+    {
+        Handles.color = Color.blue;
+        Handles.DrawWireArc(origin, Vector3.up, Vector3.forward, 360, radius);
+
+
+        Handles.color = Color.white;
+        for (int i = 1; i < Mathf.Floor((radius) / (radiusScale)); i++)
+        {
+            float radiusOffset = i * radiusScale;
+            Handles.DrawWireArc(origin, Vector3.up, Vector3.forward, 360, radiusOffset);
+        }
+
+
+        for (int i = 0; i < Mathf.Floor((180) / (angularScale)) + 1; i++)
+        {
+            float angularOffset = i * angularScale;
+            Handles.DrawLine(origin + Quaternion.Euler(0, angularOffset, 0) * Vector3.forward * radius, origin + Quaternion.Euler(0, angularOffset, 0) * Vector3.forward * -radius);
+        }
+
+        HandleUtility.Repaint();
+    }
+
+    private Vector3 GetGridOrigin()
+    {
+        switch (gridOrigin)
+        {
+            case GridOriginOptions.Selection:
+
+                Vector3 meanVector = Vector3.zero;
+
+                foreach (GameObject go in Selection.gameObjects)
+                {
+                    meanVector += go.transform.position;
+                }
+                meanVector /= Selection.gameObjects.Length;
+
+                if (gridShape == GridShapeOptions.Cartesian) return SnapVector(meanVector, gridShape);
+                return meanVector;
+
+            case GridOriginOptions.World:
+                return Vector3.zero;
+            default: return Vector3.zero;
+        }
+    }
+
+
+    private void SnapSelection(GridShapeOptions gridShape)
+    {
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            Undo.RecordObject(go.transform, "Snap Selection");
+            go.transform.position = SnapVector(go.transform.position, gridShape);
+        }
+    }
+
+    private Vector3 SnapVector(Vector3 v, GridShapeOptions gridShape)
+    {
+        switch (gridShape)
+        {
+            case GridShapeOptions.Cartesian:
+                float x = RoundToMultiple(v.x, xScale);
+                float y = RoundToMultiple(v.y, yScale);
+                float z = RoundToMultiple(v.z, zScale);
+
+                return new Vector3(x, y, z);
+
+            case GridShapeOptions.Radial:
+
+
+                float magnitude = RoundToMultiple(v.magnitude, radiusScale);
+
+                float angle = Vector3.Angle(new Vector3(v.x, 0, v.z), Vector3.forward);
+                float roundedAngle = RoundToMultiple(Mathf.Abs(angle), angularScale) * Mathf.Sign(angle);
+
+                return Quaternion.Euler(0, roundedAngle, 0) * (Vector3.forward * magnitude);
+
+            default: return Vector3.zero;
+        }
+    }
+
+    private float RoundToMultiple(float number, float multipleOf)
+    {
+        return Mathf.Round(number / multipleOf) * multipleOf;
+    }
+}
